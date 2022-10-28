122d51a1ba1fb525f1d8396d5cfb3010b8765aaf
Stefano Amodei <stefano.amodei@pm.me>
2022-10-28 17:05:08 -0400

    Removed -h. TODO work on converting from CIDR to decimal subnet mask.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
 extern crate clap;
 
-use clap::{Arg, App, SubCommand, value_t};
-/*
-fn get_broadcast(cidr: Ipv4Net) {
-    println!("{:?}", cidr);
-}
-
-fn to_decimal() {
-    println!("decimal");
-}
-*/
-/*fn get_range(cidrnet: Ipv4Net) {
-    println!("{:?}", cidrnet);
-}*/
+use clap::{Arg, App, value_t};
 
 fn get_maxhosts(maskbits: u32) -> u32 {
     // TODO Rewrite to take output from parse_addr
@@ -63,6 +51,14 @@ fn to_binary(input: Vec<u8>) -> Vec<u32> {
 
 }
 
+fn slash_to_binary(input: u32) -> Vec<u8> {
+    // From slash notation to binary.
+    let mut slashbin: Vec<u8> = vec![];
+    slashbin.push(format!("{:b}", input).parse().unwrap());
+
+    return slashbin;
+}
+
 fn get_slash(input: Vec<u32>) -> u32 {
     // Take in binary and add the ones to get slash.
     let mut convert: Vec<u32> = vec![];
@@ -83,7 +79,7 @@ fn get_slash(input: Vec<u32>) -> u32 {
 
 fn main() {
     let matches = App::new("snetcalc")
-        .version("0.0")
+        .version("0.1")
         .author("Stefano Amodei <stefano.amodei@pm.me>")
         .about("Calculates subnets.")
         .arg_required_else_help(true)
@@ -92,10 +88,6 @@ fn main() {
              .short('c')
              .long("cidr")
              .value_name("CIDR"))
-        .arg(Arg::with_name("hosts")
-             .short('h')
-             .long("hosts")
-             .value_name("hosts"))
         .arg(Arg::with_name("subnet")
              .short('s')
              .long("subnet")
@@ -107,26 +99,36 @@ fn main() {
         .get_matches();
 
     if matches.is_present("cidr") {
-        // TODO Parse CIDR
-        // snetcalc -c 10.0.0.0/24
-        // Range 10.0.0.0-10.0.0.255
-        // Max addresses
-        // Max usable hosts 254
-        // Network 10.0.0.0
-        // Broadcast 10.0.0.255
-        // Wildcard 0.0.0.255
-        // Subnet Mask 255.255.255.0
-        let cidr = value_t!(matches.value_of("cidr"), String).unwrap_or_else(|e| e.exit());
-    } else if matches.is_present("hosts") {
-        let hosts = value_t!(matches.value_of("hosts"), u32).unwrap_or_else(|e| e.exit());
-        println!("Total number of hosts: {:?}", get_maxhosts(hosts));
-        println!("Total number of usable hosts: {:?}", get_usablehosts(get_maxhosts(hosts)));
+        let input = value_t!(matches.value_of("cidr"), String).unwrap_or_else(|e| e.exit());
+
+        let slash: u32 = input.parse().unwrap();
+        // Get hosts using CIDR notation.
+        let max_hosts = get_maxhosts(slash);
+        let usable_hosts = get_usablehosts(max_hosts);
+
+        let test = slash_to_binary(slash);
+        println!("{:?}", test);
+        println!("CIDR Notation: /{:?}", input);
+        println!("Total number of hosts: {:?}", max_hosts);
+        println!("Total number of usable hosts: {:?}", usable_hosts);
     } else if matches.is_present("subnet") {
         // Convert from subnet mask to slash notation.
         let input = value_t!(matches.value_of("subnet"), String).unwrap_or_else(|e| e.exit());
 
+        // Parse input and convert from decimal subnet mask to binary.
         let binary = to_binary(parse_addr(input));
-        println!("{:?}", get_slash(binary));
+
+        // Convert binary subnet mask to CIDR notation.
+        let slash = get_slash(binary);
+
+        // Get hosts using CIDR notation.
+        let max_hosts = get_maxhosts(slash);
+        let usable_hosts = get_usablehosts(max_hosts);
+
+        println!("CIDR Notation: /{:?}", slash);
+        //println!("Wildcard Mask: {:?}", wildcard);
+        println!("Total number of hosts: {:?}", max_hosts);
+        println!("Total number of usable hosts: {:?}", usable_hosts);
     } else if matches.is_present("wildcard") {
         let input = value_t!(matches.value_of("wildcard"), String).unwrap_or_else(|e| e.exit());
         // fix this formatting
